00fce2d895d9d80966c88257ac8bf75822ec0000
Bruno Augusto <brunoaugusto.silva@yahoo.com.br>
2023-10-21 20:41:19 -0300

    Correção name input

--- php/send.php
+++ php/send.php
@@ -7,16 +7,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $mensagem = $_POST['mensagem'];
 
     // Preparar o e-mail
+    $headers = "MIME-Version: 1.1\r\n";
+    $headers .= "Content-type: text/plain; charset=UTF-8\r\n";
+    $headers .= "From: contato@brunoaugusto.com\r\n"; // remetente
+    $headers .= "Return-Path: contato@brunoaugusto.com\r\n"; // return-path
+
+
     $to = 'contato@brunoaugusto.com';
     $subject = 'Novo contato: '.$assunto;
     $message = "Nome: ".$nome."\n".
                "E-mail: ".$email."\n".
                "Telefone: ".$telefone."\n".
-               "Mensagem: ".$mensagem."\n".
-               "Se inscrever: ".$seinscrever;
+               "Mensagem: ".$mensagem."\n";
     
     // Enviar o e-mail
-    if(mail($to, $subject, $message)){
+    if(mail($to, $subject, $message, $headers)){
         echo "E-mail enviado com sucesso.";
         
         // Enviar um e-mail de confirmação para a pessoa que preencheu o formulário
@@ -24,7 +29,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $subject = 'Confirmação do contato';
         $message = "Obrigado pelo seu contato, ".$nome.". Nós recebemos sua mensagem e responderemos em breve.";
         
-        if(mail($to, $subject, $message)){
+        if(mail($to, $subject, $message, $headers)){
             echo "E-mail de confirmação enviado com sucesso.";
         } else {
             echo "Falha ao enviar o e-mail de confirmação.";
